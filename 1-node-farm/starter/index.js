4db3679797c2e19584dbd8ef3ef31a42c04fb17d
kiranvinay-p <vikiranme003@gmail.com>
2025-02-28 00:31:17 +0530

    check

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -31,7 +31,7 @@ const url = require("url");
 ///////////////////////////////
 //  SERVER
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
+// const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
